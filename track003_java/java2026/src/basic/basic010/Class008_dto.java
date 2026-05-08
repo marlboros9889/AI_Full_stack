5890562168311880cd08ef0dc283bdb21c1267ea
장영탁 <marlboros9889@gmail.com>
2026-05-08 18:14:56 +0900

    2026.05.08

---
+++ track003_java/java2026/src/basic/basic010/Class008_dto.java
@@ -0,0 +1,11 @@
+package basic.basic010;
+
+import basic.basic010_ex.Apple;
+
+//1. basic010_ex - 클래스    class Apple{  private name; private price;}
+public class Class008_dto {
+	public static void main(String[] args) {
+		Apple one = new Apple();    System.out.println(one);
+	}
+
+}
---
+++ track003_java/java2026/src/basic/basic014/Map002.java
@@ -0,0 +1,85 @@
+package basic.basic014;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Map002 {
+	public static void main(String[] args) {
+	
+		Map<String, UserDto> maps = new HashMap<>();
+		maps.put("first", new UserDto("first@mail.com"));
+		maps.put("second", new UserDto("second@mail.com"));
+		maps.put("third", new UserDto("third@mail.com"));
+		maps.put("third", new UserDto("33@mail.com"));
+		maps.put("third", new UserDto("33@mail.com"));
+		
+		System.out.println("몇명 ? " +  maps.size());  // 3
+		System.out.println("몇명 ? " + maps);
+		//third=UserDto  [no=5, email=33@mail.com]
+		//first=UserDto  [no=1, email=first@mail.com]
+		//second=UserDto [no=2, email=second@mail.com]
+		
+		//maps.entrySet() 이용해서 향상된 for로 출력
+		for( Entry<String, UserDto> u : maps.entrySet()) {
+			String key = u.getKey();
+			UserDto Value = u.getValue();
+			System.out.println("nickname : " + key + ", email :" + Value.getEmail());
+			}
+		//maps.entrySet() 이용해서 향상된 Itertor로 출력 (itretor(), hashNext(), next() )
+		Iterator<Entry<String, UserDto>> iter = maps.entrySet().iterator(); //줄서기
+		while(iter.hasNext()) { //처리대상 확인
+			Entry<String, UserDto> m = iter.next();
+			String key = m.getKey();
+			UserDto Value = m.getValue();
+			System.out.println("nicname:" + key +", email:" + Value.getEmail());
+		}
+		
+		
+	}
+
+}
+
+/*
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MapEx001 {
+    public static void main(String[] args) {
+        
+        // 1. MAP 만들기
+        Map<String, String> map = new HashMap<>();
+        map.put("피구왕", "통키");
+        map.put("제빵왕", "김탁구");
+        map.put("요리왕", "비룡");
+        
+        // 2-1. 출력
+        System.out.println("==============================");
+        System.out.println("KING\tNAME");
+        System.out.println("==============================");
+        
+        for (String king : map.keySet()) {
+            System.out.println(king + "\t" + map.get(king));
+            System.out.println("---------------------");
+        }
+        
+        // 2-2. 사용자 입력받아 값 출력
+        Scanner sc = new Scanner(System.in);
+        System.out.println("KING의 정보를 제공중입니다");
+        System.out.print("이름을 입력하세요> ");
+        String input = sc.nextLine();
+        
+        String name = map.get(input);
+        
+        if (name != null) {
+            System.out.println("\n■" + input + " : " + name);
+        } else {
+            System.out.println("존재하지 않는 KING입니다.");
+        }
+        
+        sc.close();
+    }
+}
+*/
